3e4201472349f6ff15e9146c3a28108e1763edec
Nikkolai Davenport <nikkolai@unity3d.com>
2014-10-24 14:24:18 -0700

    Use OnMouseUpAsButton to avoid click persistence bug

--- Assets/UnityAdsDemo/Scripts/UnityAdsButton.cs
+++ Assets/UnityAdsDemo/Scripts/UnityAdsButton.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.Advertisements;
 using System.Collections;
 
 public class UnityAdsButton : MonoBehaviour 
 {
 	public string zone = string.Empty;
 
-	void OnMouseDown ()
+	void Update ()
+	{
+		collider.enabled = Advertisement.isReady(zone);
+	}
+
+	void OnMouseUpAsButton ()
 	{
 		UnityAdsDemoRobust.ShowAd(zone);
 	}
