d0472214ba00756d1dbd292f27555a4bd0f0e5d4
Nikkolai Davenport <nikkolai@unity3d.com>
2014-12-05 15:41:53 -0800

    Add examples for showing ad on load

---
+++ Assets/UnityAdsDemo/JavaScript/Scripts/UnityAdsOnLoad.js
@@ -0,0 +1,51 @@
+﻿// UnityAdsButton.js - Written for Unity Ads Asset Store v1.0.4 (SDK 1.3.10)
+//  by Nikkolai Davenport <nikkolai@unity3d.com>
+//
+// A simple example for showing ads on load using the UnityAdsHelper script.
+
+#pragma strict
+
+public class UnityAdsOnLoad extends MonoBehaviour 
+{
+	public var zoneID : String = String.Empty;
+	public var timeout : float = 15.0;
+	public var disablePause : boolean;
+
+	private var _startTime : float = 0.0;
+
+#if UNITY_IOS || UNITY_ANDROID
+	function Start () : IEnumerator
+	{
+		// Check to see if Unity Ads is initialized.
+		//  If not, wait until the next frame and check again.
+		do { yield; }
+		while (!UnityAdsHelper.isInitialized());
+		
+		Debug.Log("Unity Ads has finished initializing. Waiting for ads to be ready...");
+
+		// Set a start time for the timeout.
+		_startTime = Time.timeSinceLevelLoad;
+		
+		// Check to see if Unity Ads are available and ready to be shown. 
+		//  If not, wait until the next frame and check again.
+		while (!UnityAdsHelper.isReady(zoneID))
+		{
+			if (Time.timeSinceLevelLoad - _startTime > timeout)
+			{
+				Debug.LogWarning("The process for showing ads on load has timed out. Ad not shown.");
+
+				// Break out of both this loop and the Start method; Unity Ads will not
+				//  be shown on load since the wait time exceeded the time limit.
+				return;
+			}
+
+			yield;
+		}
+		
+		Debug.Log("Ads are available and ready. Showing ad now...");
+		
+		// Show ad after Unity Ads finishes initializing and ads are ready to show.
+		UnityAdsHelper.ShowAd(zoneID,!disablePause);
+	}
+#endif
+}
